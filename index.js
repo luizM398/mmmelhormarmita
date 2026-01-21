f656ea87748bbc570eaf99eab238b2df71fa3718
luizM398 <luizmiguel398@gmail.com>
2026-01-21 12:52:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware para JSON (IMPORTANTE)
+app.use(express.json());
+
+// rota de teste
 app.get('/', (req, res) => {
   res.send('Servidor rodando');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-app.post('/webhook', express.json(), (req, res) => {
+// webhook do Mercado Pago
+app.post('/webhook', (req, res) => {
   console.log('Webhook recebido');
   console.log(req.body);
 
   res.status(200).send('ok');
+});
+
+// manter servidor vivo
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
