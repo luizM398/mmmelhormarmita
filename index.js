ec578554840b1d8187e7543d6d9fdc92b97ab7e3
luizM398 <luizmiguel398@gmail.com>
2026-02-01 16:16:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -447,8 +447,8 @@ if (cliente.estado === 'MENU') {
     }
 
     let cardapio = `🍱 *Cardápio do Dia para ${cliente.nome}*\n` +
-                   `⚖️ *Peso: 400g por marmita*\n` +
-                   `🔥 *PROMOÇÃO:* Acima de 5 unid o preço *CAI* para *R$ 17,49/un*!\n\n`;
+                  `🔥 *PROMOÇÃO:* Acima de 5 unid o preço *CAI* para *R$ 17,49/un*!\n` +
+                  `⚖️ Peso: 400g por marmita\n\n`;
     
     dados.forEach(item => { cardapio += `🔹 ${item.PRATO} – R$ 19,99\n`; });
     cardapio += `\nPara fazer seu pedido, digite *2*.\nOu digite *0* para voltar.`;
