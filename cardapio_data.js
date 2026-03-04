e5cda09d7dde32833ce1cb9b88f96cd01648f342
luizM398 <luizmiguel398@gmail.com>
2026-03-04 07:17:04 -0300

    Create cardapio_data.js

---
+++ cardapio_data.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { prato: "Arroz, strogonoff de frango e brócolis", preco: 19.99},
+  { prato: "Arroz colorido, frango grelhado e creme de milho", preco: 19.99 },  
+  { prato: "Arroz, carne desfiada e legumes salteados", preco: 19.99 },
+  { prato: "Arroz com brócolis, carne em tiras e cenoura cozida", preco: 19.99 },
+  { prato: "Escondidinho de carne a primavera", preco: 19.99 },
+  { prato: "Arroz, feijão e frango em cubos com legumes ", preco: 19.99 }
+];
