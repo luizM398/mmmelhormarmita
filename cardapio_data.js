bbddc8a3956266b3f64417d2c5b3011cb2bc7d6c
luizM398 <luizmiguel398@gmail.com>
2026-03-10 11:26:12 -0300

    Update cardapio_data.js

--- cardapio_data.js
+++ cardapio_data.js
@@ -5,4 +5,6 @@ module.exports = [
   { prato: "Arroz com brócolis, carne em tiras e cenoura cozida", preco: 19.99 },
   { prato: "Escondidinho de carne a primavera *⭐ PREMIUM*", preco: 19.99, premiun: true },
   { prato: "Arroz, feijão e frango em cubos com legumes ", preco: 15.99 }
+  { prato: "Arroz, carne de soja e brócolis com ervilha", preco: 19.99 }
+  { prato: "Arroz, almondegas fit e vagem com cenoura", preco: 19.99 }
 ];
